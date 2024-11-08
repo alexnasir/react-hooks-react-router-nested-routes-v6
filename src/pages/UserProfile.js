9861974d9d1a8c3b5729cde839b09c744feaf63d
alex <alexnasiali45@gmail.com>
2024-11-08 18:51:11 +0300

    fitst commit

--- src/pages/UserProfile.js
+++ src/pages/UserProfile.js
@@ -1,5 +1,6 @@
 import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
+import { useParams, useOutletContext } from "react-router-dom";
 
 function UserProfile() {
   const [user, setUser] = useState({});
